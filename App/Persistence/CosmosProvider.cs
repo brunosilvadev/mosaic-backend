3985f6ebc70c064caa2f5255e2e6d5e66a8661b7
Bruno Silva <43076413+brunosilvadev@users.noreply.github.com>
2022-10-25 16:37:24 -0500

    drafting cosmos db

---
+++ App/Persistence/CosmosProvider.cs
@@ -0,0 +1,35 @@
+﻿using Mosaic.Model;
+using Microsoft.Azure.Cosmos;
+
+namespace Mosaic.Persistence;
+
+public class CosmosProvider
+{
+    private IConfiguration _config;
+    private readonly string _uri;
+    private readonly string _key;
+
+    private const string _dbId = "PixelCanvas";
+    private const string _containerId = "Items";
+
+    private readonly CosmosClient _client;
+    
+    private readonly Database _db;
+
+    private Container _container;
+
+    public CosmosProvider(IConfiguration config)
+    {
+        _config = config;
+        _uri = _config.GetValue<string>("EndpointUri");
+        _key = _config.GetValue<string>("PrimaryKey");
+
+        _client = new CosmosClient(_uri, _key, new CosmosClientOptions() { ApplicationName = "CollabCanvas" });
+        _db = _client.CreateDatabaseIfNotExistsAsync(_dbId).Result;
+        _container = _db.CreateContainerIfNotExistsAsync(_containerId, "/partitionKey").Result;
+    }
+    public bool ConfigsWereRead()
+    {
+        return (_uri != null && _key != null);
+    }
+}
--- Tests/Tests/MosaicTests.cs
+++ Tests/Tests/MosaicTests.cs
@@ -1,5 +1,7 @@
 ﻿using Xunit;
 using Mosaic.Model;
+using Mosaic.Persistence;
+using Microsoft.Extensions.Configuration;
 
 namespace Mosaic.Tests;
 public class MosaicTests
@@ -9,6 +11,18 @@ public class MosaicTests
     {
         var c = new Canvas();
         Assert.IsType<List<Pixel>>(c.Pixels);
+    }
+    [Fact]
+    public void CosmosConnects()
+    {
+        var inMemorySettings = new Dictionary<string, string>
+        {
+            {"EndpointUri",@"AAA" },
+            {"PrimaryKey",@"BBB"}
+        };
+        IConfiguration config = new ConfigurationBuilder().AddInMemoryCollection(inMemorySettings).Build();                    
+        var c = new CosmosProvider(config);
+        Assert.True(c.ConfigsWereRead());
     }
 
 }
