39a1c6f62852fbb868e8c0a26c6db6a37fc72ccf
Bruno Silva <brunosilva.dev@gmail.com>
2022-12-22 15:00:50 -0600

    added Cors

--- App/Program.cs
+++ App/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Cors;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSwaggerGen();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors();
 builder.Services.AddTransient<IEndpoint, MosaicEndpoint>();
 builder.Services.AddSingleton<Mosaic.Persistence.ITemporaryDbProvider, Mosaic.Persistence.TemporaryDbProvider>();
 builder.Services.AddSingleton<Mosaic.Workers.IBrush, Mosaic.Workers.Brush>();
@@ -19,5 +22,11 @@ app.UseSwaggerUI(options =>
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
     options.RoutePrefix = string.Empty;
 });
+app.UseCors(options =>
+    options.WithOrigins("http://localhost:4200", "https://gentle-plant-06a96d810.2.azurestaticapps.net/")
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+);
+
 
 app.Run();
